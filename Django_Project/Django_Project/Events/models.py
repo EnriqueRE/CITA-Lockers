45fcbc5ec774a3ac99b319fe07d68bfaf1015d3f
Agustin de la Rocha <agustin.delarocha@gmail.com>
2013-10-02 08:32:41 -0600

    Update models.py

--- Django_Project/Django_Project/Events/models.py
+++ Django_Project/Django_Project/Events/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+class Locker(models.Model):
+    lockerUser = models.OneToOneField(User)
+    name = models.CharField(max_length=200)
+    zone = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+        
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    usergpf1 = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    locker = models.CharField(max_length=200)
+    zone = models.CharField(max_length=200)
+    date = models.CharField(max_length=200)
