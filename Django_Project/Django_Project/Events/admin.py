74cd44ec5090db67c686c4fca9fdf1a43c97d395
Agustin de la Rocha <agustin.delarocha@gmail.com>
2013-10-02 08:56:09 -0600

    Create admin.py

---
+++ Django_Project/Django_Project/Events/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from LockersApp.models import User, Locker, Event, Zone
+
+class LockerAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('name', 'zone', 'description')
+
+admin.site.register(Locker, LockerAdmin)
+
+class LockerUserAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('name', 'lastname')
+
+admin.site.register(User, LockerUserAdmin)
+
+class LockerEventAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('name', 'usergpf1','Description', 'locker', 'zone', 'date')
+
+admin.site.register(Event,LockerEventAdmin)
