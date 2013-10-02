8d88d9c048008b549e048cdcfea56171a526fe67
Agustin de la Rocha <agustin.delarocha@gmail.com>
2013-10-02 08:54:12 -0600

    Create api.py

---
+++ Django_Project/Django_Project/Events/api.py
@@ -0,0 +1,44 @@
+from tastypie import fields
+from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
+from LockersApp.models import Locker, User, Event
+
+from django.conf.urls import url
+
+class LockersResource(ModelResource):
+    class Meta:
+        queryset = Locker.objects.all()
+        resource_name = 'Locker'
+        fields = ['name', 'lastname', 'zone', 'description']
+        authorization = Authorization()
+        allowed_methods = ['get', 'post', 'delete' 'put']
+        include_resource_uri = False
+        include_absolute_uri = False
+
+class UserResource(ModelResource):
+    locker = fields.ToOneField('LockersApp.api.LockersResource', attribute='locker', related_name='locker', full=True,
+                               null=True)
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'User'
+        fields = ['name', 'lastname']
+        authorization = Authorization()
+        allowed_methods = ['get', 'post', 'delete' 'put']
+        include_resource_uri = False
+        include_absolute_uri = False
+
+class EventResource(ModelResource):
+    #close_events = fields.CharField(attribute = 'close_events')
+    #total_events = fields.IntegerField(readonly=True)
+
+    class Meta:
+        queryset = Event.objects.all().order_by('date')
+
+        resource_name = 'Event'
+        fields = ['name', 'usergpf1','Description', 'locker', 'zone', 'date', 'count_event']
+        authorization = Authorization()
+        allowed_methods = ['get', 'post', 'delete', 'put']
+        #list_allowed_methods = ['delete']
+        detail_uri_name = 'usergpf1'
+
+        filtering = {'usergpf1':['exact']}
