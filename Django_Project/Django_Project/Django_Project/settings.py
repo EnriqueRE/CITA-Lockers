7e037a2c2ad4aa9e445ce9cf6b97f647dc28c721
Enrique Ramirez <enrique.ramirez35@gmail.com>
2013-10-03 10:28:40 -0600

    Fixed admin access, removed duplicates and wrong parameters.

--- Django_Project/Django_Project/Django_Project/settings.py
+++ Django_Project/Django_Project/Django_Project/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
         'PASSWORD': 'admin',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
 }
